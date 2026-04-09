f34880da210eefd606bbe2cce82089dca48b3d37
peareon <alejandro.peraltad@outlook.com>
2026-04-09 16:15:13 -0600

    added images and data- as properties

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
 const cart = document.querySelector(".cart");
+const menu = document.querySelector(".menu");
+const closeMenu = document.querySelector(".fa-xmark");
+const cartCounter = document.querySelector(".cart__counter");
 
-const iconRemove = document.querySelectorAll(".cart__remove");
-iconRemove.forEach((element) => {
-    element.addEventListener("click", () =>{
-        element.parentElement.remove();
-    })
-} );
 
 const cartIcon = document.querySelector(".header").childNodes[5];
 cartIcon.addEventListener("click", () => {
-    console.log("clickeed")
-    console.log(cart)
     cart.classList.toggle("cart_show");
 })
 
+const menuIcon = document.querySelector(".header").childNodes[1];
+menuIcon.addEventListener("click", () => {
+    menu.classList.add("menu_show");
+})
+
+closeMenu.addEventListener("click", ()=>{
+    menu.classList.remove("menu_show");
+})
+
+
 const productsCart = document.querySelectorAll(".products__cart");
+
 productsCart.forEach((element) => {
     element.addEventListener("click", () => {
-        console.log(element);
         const src = element.parentElement.getAttribute("data-src");
         const dataAlt = element.parentElement.getAttribute("data-alt");
         const price = element.parentElement.innerText;
@@ -29,5 +34,13 @@ productsCart.forEach((element) => {
                 <p class="cart__item cart--margin">${name}</p>
                 <i class="cart__remove"><img src="imgs/quitar.png" alt="Quitar" class="cart__deleteicon"></i>
             </div>`)
+        cartCounter.innerText = (cart.childElementCount - 1)
+        let iconRemove = document.querySelectorAll(".cart__remove");
+        iconRemove.forEach((element) => {
+        element.addEventListener("click", () =>{
+        element.parentElement.remove();
+        cartCounter.innerText = (cart.childElementCount - 1)
+        })
+        } );
     })
-} )
+} );
